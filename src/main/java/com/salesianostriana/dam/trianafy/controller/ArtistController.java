cad30cc642a7ada3a71b67cd0e1cdb05577deb58
Maylor <maylorbustamante2001@gmail.com>
2022-11-23 12:49:15 +0100

    Se sube la base intento de hacer los endPoint Get

---
+++ src/main/java/com/salesianostriana/dam/trianafy/controller/ArtistController.java
@@ -0,0 +1,30 @@
+package com.salesianostriana.dam.trianafy.controller;
+
+import com.salesianostriana.dam.trianafy.model.Artist;
+import com.salesianostriana.dam.trianafy.repos.ArtistRepository;
+import com.salesianostriana.dam.trianafy.service.ArtistService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/artist")
+@RequiredArgsConstructor
+public class ArtistController {
+
+    private final ArtistService artistService;
+    private final ArtistRepository artistRepository;
+
+    @GetMapping("/")
+    public ResponseEntity<List<Artist>> getArtist() {
+        return ResponseEntity
+                .ok()
+                .body(artistService.findAll());
+
+    }
+
+}
