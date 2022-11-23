4da8b71d5b46e087518a7c336af1353a4e8dab29
Maylor <maylorbustamante2001@gmail.com>
2022-11-23 19:20:11 +0100

    Falta acabar, me paso a un ordenador mas potente

---
+++ src/main/java/com/salesianostriana/dam/trianafy/controller/ArtistController.java
@@ -0,0 +1,45 @@
+package com.salesianostriana.dam.trianafy.controller;
+
+import com.salesianostriana.dam.trianafy.model.Artist;
+import com.salesianostriana.dam.trianafy.service.ArtistService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/artist")
+public class ArtistController {
+
+    private final ArtistService artistService;
+
+
+    @GetMapping("/")
+    public ResponseEntity<List<Artist>> findAll() {
+        if (artistService.findAll().isEmpty()) {
+            return (ResponseEntity<List<Artist>>) ResponseEntity.notFound();
+        }
+        return ResponseEntity.ok(artistService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Artist> findByID(@PathVariable Long id) {
+        if (!artistService.findById(id).isPresent()) {
+            return (ResponseEntity<Artist>) ResponseEntity.notFound();
+        }
+        /**
+         *      return ResponseEntity.of(artistService.findById(id));
+         *
+         */
+        return (ResponseEntity<Artist>) ResponseEntity.notFound();
+
+
+    }
+
+}
